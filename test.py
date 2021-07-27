c455baa4d4e5b2056ae38977421fd507fb0ced76
TiMoShKa5118 <62597040+TiMoShKa5118@users.noreply.github.com>
2021-07-27 15:48:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
+import time
+
+timer = 0.00
+
 while True:	
 
 	n = int(input("Введите число "))
 
 	colvo=(len(str(n+n**2+41**n+n**2+41**n)))
 
+        while True:
+		sleep.time(0.01)
+		timer = timer + 0.01
+
 	print(n+n**2+41**n+n**2+41, colvo,"символов")
+	print(timer)

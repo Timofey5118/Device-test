b9985ac43e84c5236d7d48dd5eb341c8b7a0a919
TiMoShKa5118 <62597040+TiMoShKa5118@users.noreply.github.com>
2021-07-27 14:25:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ while True:
 
 	n = int(input("Введите число "))
 
-	colvo=(len(str(n+n**2+41**n+n**2+41)))
+	colvo=(len(str(n+n**2+41**n+n**2+41**n)))
 
 	print(n+n**2+41**n+n**2+41, colvo,"символов")
